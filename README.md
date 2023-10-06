a65e10e1807048e630c65757c1b4057d759ad98d
Eduardo Rodríguez <esanchez63@gatech.edu>
2023-10-05 21:42:40 -0400

    Build bot for slack, add gtp API interaction

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Slack-HackerNewsBot
 Slack NewsBot is an intelligent chatbot designed to bring daily news updates directly to your Slack workspace. Powered by natural language processing and personalized interactions, this chatbot not only keeps your team informed but also engages in friendly conversations.
+
+Python version: 3.11.5
+Software stack:
+	- Haxor for interacting with HN API
+	- Slackbot for interacting with Slack API
+	- EvaDB for the AI stack and relational storage
--- hacker_news_bot.py
+++ hacker_news_bot.py
@@ -1,21 +1,65 @@
-from slackbot.bot import Bot, respond_to, listen_to
-import os
-import evadb
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Configure your bot's API token
-API_TOKEN = "YOUR_SLACK_API_TOKEN"
-os.environ["OPENAI_KEY"] = "sk-..."
+import argparse, json
+import os, sys
 
-# Initialize the bot
-bot = Bot()
+import evadb, openai
 # Connect to EvaDB and get a database cursor
 cursor = evadb.connect().cursor()
 
-@listen_to('.*')  # Matches any message
-def handle_message(message):
-    pass
+JSON_FILE_PATH = None
 
+# Load API KEYS from json file
+# Format = { "API" = "KEY", ... }
+def load_api_keys_from_json(file_path):
+    try:
+        with open(file_path, 'r') as json_file:
+            api_keys = json.load(json_file)
+            for key, value in api_keys.items():
+                os.environ[key] = value
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        sys.exit();
+    except json.JSONDecodeError:
+        print(f"Error parsing JSON in '{file_path}'.")
+        sys.exit();
 
+def parse_arguments():
+    # JSON for API Keys 
+    global JSON_FILE_PATH
+    parser = argparse.ArgumentParser(description="Load API keys from a JSON file and set them as environment variables.")
+    parser.add_argument("-i", "--input", help="Path to the JSON file containing API keys", required=True)
+
+    args = parser.parse_args()
+    JSON_FILE_PATH = args.input
+
+# Get the API tokens for config
+parse_arguments()
+load_api_keys_from_json(JSON_FILE_PATH)
+
+# Initialize the bot
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Respond to any messages
+@app.message(".*")
+def message_hello(message, say):
+    # Set OpenAI key.
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    say(
+        openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": message["text"]}]
+            )
+        .choices[0]
+        .message.content
+        )
 
 if __name__ == "__main__":
-    bot.run()
+
+    # Inialize the app
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+    # Run the bot
+    app.run(debug=True)
+
